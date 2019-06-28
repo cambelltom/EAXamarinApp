43da04000ddf7d5fc2b44cced958b325b54cedab
Tom Evon <TOEV@bang-olufsen.dk>
2019-06-28 15:57:41 +0200

    initial commit

---
+++ CrossPlatformTest/AppInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatformTest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            //if (platform == Platform.Android)
+            //{
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.companyname.EAXamarinApp")
+                    //.ApkFile(@"C:\Users\toev\Downloads\Xamarin.UITest-master\EAXamarinApp\EAXamarinApp.Android\bin\Debug\com.companyname.EAXamarinApp-Signed.apk")
+                    .DeviceSerial("2663afc820057ece")
+                    //.DeviceSerial("712KPSL1234014")
+                    .EnableLocalScreenshots()
+                    .PreferIdeSettings()
+                    .StartApp();
+            //}
+
+            //return ConfigureApp
+            //    .iOS
+            //    .StartApp();
+        }
+    }
+}
+
